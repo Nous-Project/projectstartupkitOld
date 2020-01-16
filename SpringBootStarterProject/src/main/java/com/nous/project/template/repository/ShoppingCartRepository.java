a1d952b68b41db6191208c0c30650996e08c3a8c
sachinsfirst <sachinsfirst@mail.com>
2020-01-16 19:02:18 +0530

    Removed Unsed imports & provided temp fix to ApiErrorResponse.java

--- SpringBootStarterProject/src/main/java/com/nous/project/template/repository/ShoppingCartRepository.java
+++ SpringBootStarterProject/src/main/java/com/nous/project/template/repository/ShoppingCartRepository.java
@@ -1,14 +1,11 @@
 package com.nous.project.template.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nous.project.template.domain.ShoppingCart;
 
-import java.util.List;
-
-/**
- * Created by sahan on 4/9/2016.
- */
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    List<ShoppingCart> findByStatus(String status);
+	List<ShoppingCart> findByStatus(String status);
 }
--- SpringBootStarterProject/src/main/java/com/nous/project/template/repository/UserRepository.java
+++ SpringBootStarterProject/src/main/java/com/nous/project/template/repository/UserRepository.java
@@ -4,8 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.nous.project.template.domain.User;
 
-/**
- * Created by sahan on 4/8/2016.
- */
 public interface UserRepository extends CrudRepository<User, Long> {
 }
